264b3a322c0230201d4898d0e6fced7a66162bf3
anisaboom <al318@greenwich.ac.uk>
2017-04-02 18:32:10 +0100

    Add project files.

---
+++ FYP2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FYP2.Startup))]
+namespace FYP2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
